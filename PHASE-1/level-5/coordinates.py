f1cc83575f14eab643d3cd11bd821834f4cd0498
77anuj77 <anujparoha07@gmail.com>
2026-08-17 23:05:59 +0530

    coordinate logic with validation

---
+++ PHASE-1/level-5/coordinates.py
@@ -0,0 +1,29 @@
+# check= lambda x,y : "(x,y) lies on x-axis" if y==0 and x!= 0 else("(x,y) lies on y-axis" if x==0 and y!=0 else ("(x,y) lies at origin" if x==0 and y==0 else "Lies on cartisean plain"))
+from pydantic import BaseModel, ValidationError
+from typing import Optional, List, Dict, Literal
+
+
+import pandas as pd
+class coordinate(BaseModel):
+    x: int
+    y: int
+
+def check(coordinate : coordinate) -> None:
+    if coordinate[0] == 0 and coordinate[1] == 0:
+        print("It is on the origin")
+    elif coordinate[0] == 0 and coordinate[1] != 0:
+        print("It is on the Y-Axis")
+    elif coordinate[1] == 0 and coordinate[0] != 0:
+        print("It is on the X-Axis")
+    else:
+        print("It does not lie on origin or any of the axes")
+
+
+
+while True:
+    a= float(input("Enter the a: "))
+    b= float(input("Enter the b: "))
+
+    check((a,b))
+
+
