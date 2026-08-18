2817ef09a6dc1264ba216471462857c8b96150bd
77anuj77 <anujparoha07@gmail.com>
2026-08-18 19:08:39 +0530

    valid digit between (1-9)

---
+++ PHASE-1/level4/words.py
@@ -0,0 +1,9 @@
+words = ["Zero", "One", "Two", "Three", "Four",
+         "Five", "Six", "Seven", "Eight", "Nine"]
+
+n = int(input("Enter a digit (0-9): "))
+
+if 0 <= n <= 9:
+    print(words[n])
+else:
+    print("Invalid digit")
